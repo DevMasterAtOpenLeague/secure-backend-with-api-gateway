85d992bca77cbdca8607cc0932d15f9e970b534f
OL Dev <OL Dev>
2016-01-30 21:25:43 -0500

    Auth Server Template created.

---
+++ Auth Server/src/main/java/com/romaine/backend/AuthserverApplication.java
@@ -0,0 +1,15 @@
+package com.romaine.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SpringBootApplication
+@EnableAuthorizationServer
+public class AuthserverApplication extends WebMvcConfigurerAdapter{
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthserverApplication.class, args);
+	}
+}
