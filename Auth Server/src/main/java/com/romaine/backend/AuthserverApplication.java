d130045c4feb367b788ff087051cac5bb96a6331
OL Dev <OL Dev>
2016-02-01 18:08:54 -0500

    Added userInfoUri

--- Auth Server/src/main/java/com/romaine/backend/AuthserverApplication.java
+++ Auth Server/src/main/java/com/romaine/backend/AuthserverApplication.java
@@ -1,5 +1,7 @@
 package com.romaine.backend;
 
+import java.security.Principal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
@@ -17,7 +19,9 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -28,10 +32,16 @@ import com.mongodb.MongoClientURI;
 @SpringBootApplication
 @RestController
 @EnableAuthorizationServer
+@EnableResourceServer
 public class AuthserverApplication extends WebMvcConfigurerAdapter implements CommandLineRunner{
 
 	@Autowired
 	UserRepository userRepository;
+	
+	@RequestMapping("/user")
+	public Principal user(Principal user){
+		return user;
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(AuthserverApplication.class, args);
